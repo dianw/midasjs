e9188aad601e7943b2d3552e2f9dcfd998dbd36a
dianw <dian.aditya@meruvian.org>
2014-11-25 16:04:55 +0700

    Fix change topnav color on link changed

--- app/scripts/controllers/topnav.js
+++ app/scripts/controllers/topnav.js
@@ -7,7 +7,18 @@
  * # TopnavCtrl
  * Controller of the hoqiiApp
  */
-angular.module('hoqiiApp').controller('topNavCtrl', function ($scope, topNavService) {
+ /* global $:false */
+angular.module('hoqiiApp').controller('topNavCtrl', function ($scope, topNavService, $location) {
+	$scope.$on('$routeChangeStart', function() { 
+		if($location.path() !== '/') {
+			var element = $('[dynamic-top-nav]');
+			element.addClass( 'navbar-scroll' );
+            element.find('#buttom-logo').show();
+            element.find('#buttom-logo').css('height','36px');
+            element.find('#top-logo').hide();
+		}
+ 	});
+
     topNavService.getMenus().success(function(data) {
     	$scope.menus = data;
     });
